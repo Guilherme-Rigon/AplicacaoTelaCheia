e71860941c9433c5293a78a52f3fb4ea298204a0
Guilherme Luiz <guilherme.luiz.muleque@hotmail.com>
2021-07-08 21:06:38 -0300

    Initial Commit

---
+++ TelaUnica/wwwroot/js/site.js
@@ -0,0 +1,31 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+//function TrocarPrivacy() {
+//    $.ajax({
+//        url: "/Home/Privacy",
+//        success: function (result) {
+//            $("#main").html(result);
+//        }
+//    })
+//}
+//function TelaCheia() {
+//    var elem = document.documentElement;
+//    if (elem.requestFullscreen) {
+//        elem.requestFullscreen();
+//    } else if (elem.webkitRequestFullscreen) {
+//        elem.webkitRequestFullscreen();
+//    } else if (elem.msRequestFullscreen) {
+//        elem.msRequestFullscreen();
+//    }
+//}
+//function TrocarIndex() {
+//    $.ajax({
+//        url: "/Home/Index",
+//        success: function (result) {
+//            var response = $($.parseHTML(result)).filter("#divContainer")
+//            $("#main").html(response);
+//        }
+//    })
+//}
