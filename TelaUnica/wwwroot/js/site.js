7c771a795f4d61322fc1ecf6ef968ecc53338e4e
Guilherme Luiz <guilherme.luiz.muleque@hotmail.com>
2021-07-29 21:41:37 -0300

    Implementando um temporizador simples para atuar em ambas as páginas.

--- TelaUnica/wwwroot/js/site.js
+++ TelaUnica/wwwroot/js/site.js
@@ -1,31 +1,57 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿/// <summary>
+/// Variável booleana que define se o temporizador deve ou não ser resetado no próximo loop.
+/// </sumary >
+var resetar = false;
+/// <summary>
+/// Variável booleana que guarda o status de ativo ou inativo para o temporizador.
+/// </sumary >
+var temporizadorAtivo = true;
+/// <summary>
+/// Variável interna para o controle do interval.
+/// </sumary >
+var interval;
+/// <summary>
+/// Tempo máximo do temporizador, definir um valor para esta variável fará com que o temporizador comece do valor definido.
+/// </sumary >
+var countMaximo = 60
+/// <summary>
+/// Tempo atual do temporizador. Usado internamente.
+/// </sumary >
+var count = countMaximo;
 
-// Write your JavaScript code.
-//function TrocarPrivacy() {
-//    $.ajax({
-//        url: "/Home/Privacy",
-//        success: function (result) {
-//            $("#main").html(result);
-//        }
-//    })
-//}
-//function TelaCheia() {
-//    var elem = document.documentElement;
-//    if (elem.requestFullscreen) {
-//        elem.requestFullscreen();
-//    } else if (elem.webkitRequestFullscreen) {
-//        elem.webkitRequestFullscreen();
-//    } else if (elem.msRequestFullscreen) {
-//        elem.msRequestFullscreen();
-//    }
-//}
-//function TrocarIndex() {
-//    $.ajax({
-//        url: "/Home/Index",
-//        success: function (result) {
-//            var response = $($.parseHTML(result)).filter("#divContainer")
-//            $("#main").html(response);
-//        }
-//    })
-//}
+/// <summary>
+/// Inicia o temporizador e exibe o valor em tela no elemento com id 'temporizador'
+/// </sumary >
+function Temporizador() {
+    if (resetar) { //Verifica se o temporizador deve ser resetado;
+        clearInterval(interval); //Limpa o interval para resetar o interval;
+        resetar = false; //Marca a variavel reset como false para o ciclo continuar;
+    }
+    interval = setInterval(() => { //Cria o interval que será executado de 1 em 1 segundo;
+        if (temporizadorAtivo) { //Verifica se o temporizador está ativo;
+            count -= 1; //Diminui 1 do tempo;
+            console.log(count)
+            $("#temporizador").text(count); //Mostra o tempo na tela;
+            if (count == 0) //Verifica se o tempo acabou;
+                temporizadorAtivo = false; //Se sim desabilita o temporizador;
+        } else {
+            clearInterval(interval) //Caso o temporizador esteja desabilitado o interval é limpo;
+        }
+    }, 1000);
+}
+/// <summary>
+/// Reseta o temporizador para o tempo máximo definido
+/// </sumary >
+function ResetarTemporizador() {
+    $("#temporizador").text(countMaximo);
+    resetar = true;
+    count = countMaximo
+}
+
+/// <summary>
+/// Recebe um status booleano para ativar ou desativar o temporizador
+/// </sumary >
+/// <param name="status">status booleano a ser recebido</param>
+function TemporizadorAtivo(status) {
+    temporizadorAtivo = status;
+}
